cbe36d33ac74df3598d34f86a2abdbb2412602ba
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-02 19:00:45 +0600

    navbar

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-gradient-to-r from-[#672280] to-[#A626D3] py-4 px-7'>
+            <h1 className='text-2xl font-bold text-white'>Meme Generator</h1>
+        </div>
+    );
+};
+
+export default Navbar;
